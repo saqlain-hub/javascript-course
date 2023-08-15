9d41c3c78981892dc733fb513ccf642c652eeebd
saqlain-hub <saqlainsj12@gmail.com>
2023-08-16 03:08:10 +0500

    DOM traversing

--- 05-document-object-model/04-dom-selectors-multiple-elements/shopping-list/script1.js
+++ 05-document-object-model/04-dom-selectors-multiple-elements/shopping-list/script1.js
@@ -1,18 +1,32 @@
 // querySelectorAll()
 
-const listItems = document.querySelectorAll(".item");
-console.log(listItems[1].innerText);
+// const listItems = document.querySelectorAll(".item");
+// console.log(listItems[1].innerText);
 
-listItems[1].style.color = "red";
+// listItems[1].style.color = "red";
 
-listItems.forEach((item, index) => {
-  item.style.color = "red";
+// listItems.forEach((item, index) => {
+//   item.style.color = "red";
 
-  if (index === 1) {
-    item.remove();
-  }
+//   if (index === 1) {
+//     item.remove();
+//   }
 
-  if (index === 0) {
-    item.innerText = "Oranges";
-  }
-});
+//   if (index === 0) {
+//     item.innerText = "Oranges";
+//   }
+// });
+
+// const listItems2 = document.getElementsByClassName("item");
+
+// console.log(listItems2[2].innerText);
+
+// const listItemsArray = Array.from(listItems2);
+
+// listItemsArray.forEach((item) => {
+//   console.log(item.innerText);
+// });
+
+// const listItems3 = document.getElementsByTagName("li");
+
+// console.log(listItems3[0].innerText);
