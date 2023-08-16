34fe7073904781dabdd517353f03ea01436426a6
saqlain-hub <saqlainsj12@gmail.com>
2023-08-17 00:39:07 +0500

    create elements and append to the DOM

---
+++ 05-document-object-model/06-create-elements/shopping-list/script1.js
@@ -0,0 +1,8 @@
+const div = document.createElement("div");
+div.className = "my-element";
+div.id = "my-elements";
+div.setAttribute("title", "My element");
+
+div.innerText = "Hello World";
+
+console.log(div);
