8cb2a9e1dfceccee8ccada5e42ab95b3b26d497e
saqlain-hub <saqlainsj12@gmail.com>
2023-08-21 02:33:04 +0500

    from submission, event bubbling, event delegation

--- 06-events/07-form-submission/shopping-list/script1.js
+++ 06-events/07-form-submission/shopping-list/script1.js
@@ -3,7 +3,32 @@ const form = document.getElementById("item-form");
 function onSubmit(e) {
   e.preventDefault();
 
-  console.log("submit");
+  const item = document.getElementById("item-input").value;
+  const priority = document.getElementById("priority-input").value;
+
+  if (item === "" || priority === "0") {
+    alert("Please fill in all fields");
+    return;
+  }
+
+  console.log(item, priority);
 }
 
-form.addEventListener("submit", onSubmit);
+function onSubmit2(e) {
+  e.preventDefault();
+
+  const formData = new FormData(form);
+
+  const item = formData.get("item");
+  const priority = formData.get("priority");
+
+  const entries = formData.entries();
+
+  // console.log(item, priority);
+  // console.log(entries);
+  for (let entry of entries) {
+    console.log(entry[0]);
+  }
+}
+
+form.addEventListener("submit", onSubmit2);
