2ebba38da1d8c6e54c36ed1fa7ea5d4c18afbc94
saqlain-hub <saqlainsj12@gmail.com>
2023-08-24 15:48:28 +0500

    events and form submission complete

--- 06-events/10-window-events/script1.js
+++ 06-events/10-window-events/script1.js
@@ -1,22 +1,50 @@
-// Event delegation
+// window.onload = function () {
+//   document.querySelector("h1").textContent = "Hello World";
+// };
 
-const listItems = document.querySelectorAll("li");
-const list = document.querySelector("ul");
-
-// listItems.forEach((item) => {
-//   item.addEventListener("click", (e) => {
-//     e.target.remove();
-//   });
+// window.addEventListener("load", () => {
+//   document.querySelector("h1").textContent = "Hello World";
 // });
 
-list.addEventListener("click", (e) => {
-  if (e.target.tagName === "LI") {
-    e.target.remove();
+// window.addEventListener(
+//   "DOMContentLoaded",
+//   () => (document.querySelector("h1").textContent = "Hello World")
+// );
+
+window.addEventListener("load", () => console.log("Page Loaded"));
+
+window.addEventListener("DOMContentLoaded", () => console.log("DOM Loaded"));
+
+console.log("Run me");
+
+document.querySelector("h1").innerText = "Hello World";
+
+window.addEventListener("resize", () => {
+  document.querySelector(
+    "h1"
+  ).innerText = `Resized to ${window.innerWidth} x ${window.innerHeight}`;
+});
+
+window.addEventListener("scroll", () => {
+  console.log(`Scrolled: ${window.scrollX} x ${window.scrollY}`);
+
+  if (window.scrollY > 70) {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+  } else {
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
   }
 });
 
-list.addEventListener("mouseover", (e) => {
-  if (e.target.tagName === "LI") {
-    e.target.style.color = "red";
-  }
+window.addEventListener("focus", () => {
+  document.querySelectorAll("p").forEach((p) => {
+    p.style.color = "blue";
+  });
+});
+
+window.addEventListener("blur", () => {
+  document.querySelectorAll("p").forEach((p) => {
+    p.style.color = "black";
+  });
 });
